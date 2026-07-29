12ea4124787d18e9eb81850c8464079546e2c414
BudgetArms <61605284+PolleDesutter@users.noreply.github.com>
2026-07-29 21:24:36 +0200

    Created BlackboardKeys

---
+++ Plugins/GameFeatures/DesutterPolleZombie/Source/DesutterPolleZombieRuntime/Public/StateTree/BlackboardKeys.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace BlackboardKeys
+{
+	inline const FName Self = "SelfActor";
+	inline const FName ClosestEnemyPosition = "ClosestEnemyPosition";
+	inline const FName WeaponsInfo = "WeaponsInfo";
+	inline const FName HousesInfo ="HousesInfo";
+	inline const FName PurgeZonesInfo="PurgeZoneInfo";
+	inline const FName HaveFood= "bHaveFood";
+	inline const FName HaveMedKit="bHaveMedKit";
+	inline const FName IsEnemyClose="bIsEnemyClose";
+	inline const FName HasSeenEnemyRecently="bSeenEnemyRecently";
+	inline const FName MaxRecentEnemyTime="MaxRecentEnemyTime";
+	inline const FName TimeSinceLastSeenEnemy="TimeSinceLastSeenEnemy";
+};
