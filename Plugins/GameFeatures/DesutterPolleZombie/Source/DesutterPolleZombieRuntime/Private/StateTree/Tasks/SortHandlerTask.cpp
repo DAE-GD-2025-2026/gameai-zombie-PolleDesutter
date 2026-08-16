d99a5a0a554bb108bd685993b0d7bd5dee4758e7
BudgetArms <61605284+PolleDesutter@users.noreply.github.com>
2026-08-16 17:13:51 +0200

    Renamed SortInventory Task to InventoryHandler Task

---
+++ Plugins/GameFeatures/DesutterPolleZombie/Source/DesutterPolleZombieRuntime/Private/StateTree/Tasks/SortHandlerTask.cpp
@@ -0,0 +1,169 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StateTree/Tasks/SortHandlerTask.h"
+
+#include "StateTreeExecutionContext.h"
+
+#include "HelperFunctions.h"
+#include "Common/InventoryComponent.h"
+
+
+
+EStateTreeRunStatus FSortHandlerTask::EnterState(FStateTreeExecutionContext& Context,
+                                                   const FStateTreeTransitionResult& Transition) const
+{
+	if (!bShouldCallTick)
+	{
+		HelperFunctions::LogError("Inventory: Failed To Enable Tick");
+		return EStateTreeRunStatus::Failed;	
+	}
+	
+	HelperFunctions::LogSuccess("Inventory: Enter State");
+	
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	if (!InstanceData.Inventory)
+	{
+		HelperFunctions::LogError("Inventory: Inventory Is Null");
+		return EStateTreeRunStatus::Failed;	
+	}
+	
+	if (IsInventoryEmpty(*InstanceData.Inventory))
+	{
+		HelperFunctions::LogWarning("Inventory: Inventory Is Empty");
+		return EStateTreeRunStatus::Running;
+	}
+
+
+	return EStateTreeRunStatus::Running;
+}
+
+void FSortHandlerTask::ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	HelperFunctions::LogSuccess("Inventory: Exit State");
+	Super::ExitState(Context, Transition);
+}
+
+EStateTreeRunStatus FSortHandlerTask::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	
+	// CleanUp Inventory
+	if (IsInventoryEmpty(*InstanceData.Inventory))
+	{
+		HelperFunctions::LogVerbose("Inventory: Empty");
+		return EStateTreeRunStatus::Running;
+	}
+	
+	RemoveGarbage(*InstanceData.Inventory);
+	
+	
+	
+	// Grab Item	
+	if (!InstanceData.AcquiredItem || !Cast<ABaseItem>(InstanceData.AcquiredItem))
+	{
+		HelperFunctions::LogWarning("Inventory: Acquired Item is Null");
+		return EStateTreeRunStatus::Running;
+	}
+	
+	ABaseItem* AcquiredItem = Cast<ABaseItem>(InstanceData.AcquiredItem);
+	HelperFunctions::LogError("Inventory: Acquired Item is " + AcquiredItem->GetName());	
+	
+	
+	return EStateTreeRunStatus::Running;
+}
+
+bool FSortHandlerTask::IsInventoryEmpty(const UInventoryComponent& Inventory)
+{
+	for (const ABaseItem* Item :  Inventory.GetInventory())
+	{
+		if (Item)
+		{
+			return false;
+		}
+	}
+	
+	return true;	
+}
+
+bool FSortHandlerTask::IsInventoryFull(const UInventoryComponent& Inventory)
+{
+	for (const ABaseItem* Item :  Inventory.GetInventory())
+	{
+		if (!Item)
+		{
+			return false;
+		}
+	}
+	
+	return true;	
+}
+
+std::set<int> FSortHandlerTask::GetItemSlotsByType(const UInventoryComponent& Inventory, EItemType ItemType)
+{
+	std::set<int> ItemSlotsSet{};
+	
+	TArray<ABaseItem*> InventoryArray = Inventory.GetInventory();
+	for (int Index{}; Index < InventoryArray.Num(); ++Index)
+	{
+		const ABaseItem* Item = InventoryArray[Index];
+		if (!Item)
+		{
+			continue;
+		}
+		
+		if (ItemType == Item->GetItemType())
+		{
+			ItemSlotsSet.insert(Index);
+		}
+		
+	}
+	
+	return ItemSlotsSet;
+}
+
+std::optional<int> FSortHandlerTask::GetFreeItemSlot(const UInventoryComponent& Inventory)
+{
+	const std::set<int> FreeItemSlotsSet = GetFreeItemSlots(Inventory);
+	if (FreeItemSlotsSet.empty())
+	{
+		return std::nullopt;
+	}
+	
+	return *FreeItemSlotsSet.begin();
+}
+
+std::set<int> FSortHandlerTask::GetFreeItemSlots(const UInventoryComponent& Inventory)
+{
+	std::set<int> ItemSlotsSet{};
+	
+	TArray<ABaseItem*> InventoryArray = Inventory.GetInventory();
+	for (int Index{}; Index < InventoryArray.Num(); ++Index)
+	{
+		if (!InventoryArray[Index])
+		{
+			ItemSlotsSet.insert(Index);
+		}
+	}
+	
+	return ItemSlotsSet;
+}
+
+void FSortHandlerTask::RemoveGarbage(UInventoryComponent& Inventory)
+{
+	const std::set<int> GarbageItemSlotsSet = GetItemSlotsByType(Inventory, EItemType::Garbage);
+	if (!GarbageItemSlotsSet.empty())
+	{
+		HelperFunctions::LogSuccess("Sort Inventory: No Garbage found");
+	}
+	
+	for (const int GarbageItemSlot: GarbageItemSlotsSet)
+	{
+		FString Test = "wow" + GarbageItemSlot;
+		HelperFunctions::LogSuccess("Sort Inventory: Remove Garbage");
+		Inventory.RemoveItem(GarbageItemSlot);
+	}
+	
+}
+
+
--- Plugins/GameFeatures/DesutterPolleZombie/Source/DesutterPolleZombieRuntime/Public/StateTree/Tasks/SortHandlerTask.h
+++ Plugins/GameFeatures/DesutterPolleZombie/Source/DesutterPolleZombieRuntime/Public/StateTree/Tasks/SortHandlerTask.h
@@ -2,29 +2,35 @@
 
 #pragma once
 
+#include <set>
+
 #include "CoreMinimal.h"
 #include "StateTree/Tasks/BaseTask.h"
-#include "SortInventoryTask.generated.h"
+#include "SortHandlerTask.generated.h"
 
 
+enum class EItemType : uint8;
 class UInventoryComponent;
 
 USTRUCT()
-struct FSortInventoryTaskInstanceData : public FBaseTaskInstanceData
+struct FInventoryHandlerTaskInstanceData : public FBaseTaskInstanceData
 {
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, Category = "Input")
 	UInventoryComponent* Inventory{};
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	 AActor* AcquiredItem{};
 };
 
 
 USTRUCT()
-struct DESUTTERPOLLEZOMBIERUNTIME_API FSortInventoryTask final : public FBaseTask
+struct DESUTTERPOLLEZOMBIERUNTIME_API FSortHandlerTask final : public FBaseTask
 {
 	GENERATED_BODY()
 	
-	using FInstanceDataType = FSortInventoryTaskInstanceData;
+	using FInstanceDataType = FInventoryHandlerTaskInstanceData;
 	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
 	
 	
@@ -39,5 +45,13 @@ struct DESUTTERPOLLEZOMBIERUNTIME_API FSortInventoryTask final : public FBaseTas
 	
 private:
 	static bool IsInventoryEmpty(const UInventoryComponent& Inventory);
+	static bool IsInventoryFull(const UInventoryComponent& Inventory);
+	
 
+	static std::set<int> GetItemSlotsByType(const UInventoryComponent& Inventory, EItemType ItemType);
+	static std::optional<int> GetFreeItemSlot(const UInventoryComponent& Inventory);
+	static std::set<int> GetFreeItemSlots(const UInventoryComponent& Inventory);
+
+	static void RemoveGarbage(UInventoryComponent& Inventory);
+	
 };
